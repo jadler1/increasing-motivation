36878855a07d563e99376c3d2141297cc2e41004
jadler1 <jacksonadler2020@u.northwestern.edu>
2018-03-04 21:42:00 -0600

    Dynamic time fill

--- Scripts/calendar.js
+++ Scripts/calendar.js
@@ -1,9 +1,11 @@
 var tasks = [];
+
 if(localStorage.getItem("tasks")){
   tasks = JSON.parse(localStorage.getItem("tasks"));
 }
 var weekdays= ["sun","mon","tue","wed","thu","fri","sat"];
-
+var timetable = document.getElementById("times");
+fillCalendarTimes();
 const currentDate = new Date();
 var year = currentDate.getFullYear();
 var monthno= currentDate.getMonth();
@@ -33,6 +35,43 @@ for(var i = 0; i < 7;i++){
     j=0;
   }
   j++;
+}
+
+tasks.forEach(function(t){
+  if((t[1]-currentDate.getTime())>604800000){//if not this week
+    return;
+  }
+  var date = new Date(t[1]);
+
+})
+
+
+
+
+
+function fillCalendarTimes(){
+  var calhtml = "";
+  for(var i = 0; i <24;i++){
+    var j = (i%12);
+    if(j==0){j=12;}
+    var ampm = "AM";
+    if(i>11){
+      ampm="PM";
+    }
+    calhtml +="<tr id=\""+j+ampm+"\">";
+    calhtml +="<td class=\"hour\" rowspan=\"2\"><span>" +j +" "+ampm;
+    calhtml +="</span></td>";
+    for(var k = 0; k <7; k++){
+      calhtml +="<td class=\""+k+"\"></td>"
+    }
+    calhtml +="</tr><tr id=\""+j+"30"+ampm+"\">";
+    for(var k = 0; k <7; k++){
+      calhtml +="<td class=\""+k+"\"></td>"
+    }
+    calhtml+="</tr>";
+
+  }
+  timetable.innerHTML=calhtml;
 }
 
 
